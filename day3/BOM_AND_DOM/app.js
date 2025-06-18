df9b63f6c5fc73a46c3d4e73bbe1edb26880cd95
Probodh <probodhsahoo08@gmail.com>
2025-06-18 11:26:39 +0530

    Add initial HTML and JavaScript files for BOM and DOM exercises; implement basic heading retrieval in app.js and structure for recipe data in hw.js

---
+++ day3/BOM_AND_DOM/app.js
@@ -0,0 +1,5 @@
+// console.dir(window);
+// console.dir(window.document);
+// console.dir(document.children[0].children[1].children[0]);
+const headings=document.getElementsByTagName("h1");
+console.log("headings:",headings);
---
+++ day3/forloop/app.js
@@ -0,0 +1,21 @@
+// console.log("Hello");
+
+// const arr=["cat","dog","elephant","tiger"] ;
+
+// for(let i=0;i<arr.length;i++){
+//     const elem =arr[i];
+//     console.log(elem,i);
+// }
+
+// for(let i in arr){
+//     const elem=arr[i];
+//     console.log(elem,i);
+// }
+// for(let i of arr){
+//     console.log(i);
+// }
+// const myFunc=()=>{
+//     console.log("Hello");
+// }
+// arr.forEach(()=>{console.log(arr[0])});
+//==========================================================================================================
